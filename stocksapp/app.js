a3e10772e7868585d49a6ed41ee9c642b694fd2c
amansingh-bib <105208786+amansingh-bib@users.noreply.github.com>
2022-07-07 19:42:58 +0530

    Push changes

--- stocksapp/app.js
+++ stocksapp/app.js
@@ -1,12 +1,8 @@
-const http = require('http');
-const port = process.env.PORT || 3000
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
-});
-
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
+var express = require("express");
+var app = express();
+app.get("/url", (req, res, next) => {
+    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+   });
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
 });
